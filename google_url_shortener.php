df3045a530d44fcf7f98264f094e064c13b49dac
Fabrício Ferracioli <fabricioferracioli@gmail.com>
2011-04-19 12:13:04 -0300

    first commit with the short and expand functions from the api

---
+++ google_url_shortener.php
@@ -0,0 +1,77 @@
+<?php
+
+    class GoogleUrlShortenerComponent extends Object
+    {
+        var $name = 'GoogleUrlShortener';
+
+        public $apiKey;
+
+        public $shortenerUrl;
+
+        function initialize(&$controller, $settings = array())
+        {
+            $this->controller =& $controller;
+            $this->shortenerUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=';
+
+            if (!empty($settings))
+            {
+                if (!empty($settings['apiKey']))
+                {
+                    $this->apiKey = $settings['apiKey'];
+                }
+            }
+        }
+
+        function startup(&$controller)
+        {
+        }
+
+        function beforeRender(&$controller)
+        {
+        }
+
+        function generateShortUrl($longUrl = null)
+        {
+            if (!empty($longUrl))
+            {
+                App::import('Core', 'HttpSocket');
+                $socket = new HttpSocket();
+
+                $result = $socket->post(
+                    $this->shortenerUrl . $this->apiKey,
+                    json_encode(array('longUrl' => $longUrl)),
+                    array('header' => array('Content-Type' => 'application/json'))
+                );
+
+                return json_decode($result, true);
+            }
+            return false;
+        }
+
+        function getOriginalUrl($shortUrl = null)
+        {
+            if (!empty($shortUrl))
+            {
+                App::import('Core', 'HttpSocket');
+                $socket = new HttpSocket();
+
+                $result = $socket->get(
+                    $this->shortenerUrl . $this->apiKey,
+                    array('shortUrl' => $shortUrl)
+                );
+
+                return json_decode($result, true);
+            }
+            return false;
+        }
+
+        function shutdown(&$controller)
+        {
+        }
+
+        function beforeRedirect(&$controller, $url, $status = null, $exit = true)
+        {
+        }
+    }
+
+?>
